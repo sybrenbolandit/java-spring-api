1e79b8f856c534f2d1af53a31f83d950585ea21b
sybrenbolandit <sybrenbolandit@gmail.com>
2020-02-06 20:14:39 +0100

    Add api documentation

--- src/main/java/org/shboland/PersonController.java
+++ src/main/java/org/shboland/PersonController.java
@@ -1,13 +1,15 @@
 package org.shboland;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PersonController {
 
-    @RequestMapping("")
+    @GetMapping("")
+    @Operation(tags = "Person", description = "Get a greeting")
     public String greeting(@RequestParam(value="name", defaultValue = "you") String name) {
         return "Hello " + name;
     }
--- src/main/java/org/shboland/SpringApiApplication.java
+++ src/main/java/org/shboland/SpringApiApplication.java
@@ -1,8 +1,11 @@
 package org.shboland;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info = @Info(title = "Java Spring API", version = "1.0.0"))
 @SpringBootApplication
 public class SpringApiApplication {
 
