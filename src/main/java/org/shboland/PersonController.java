c7ae7a05f56fbb7f72e7e785fc4a91d471a92c03
sybrenbolandit <sybrenbolandit@gmail.com>
2021-08-20 20:13:52 +0200

    feat: Save person object in connected mysql database

--- src/main/java/org/shboland/PersonController.java
+++ src/main/java/org/shboland/PersonController.java
@@ -1,14 +1,27 @@
 package org.shboland;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.shboland.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PersonController {
 
-    @RequestMapping("")
-    public String greeting(@RequestParam(value="name", defaultValue = "you") String name) {
-        return "Hello " + name;
+    @Autowired
+    private PersonRepository personRepository;
+
+    @PostMapping("/persons")
+    public Person createPerson(@RequestBody Person person) {
+        return personRepository.save(person);
+    }
+
+    @GetMapping("/persons/{personId}")
+    public Person getPerson(@PathVariable String personId) {
+        return personRepository.findById(Integer.valueOf(personId))
+                .orElseThrow(() -> new RuntimeException("No person found for id: " + personId));
     }
 }
